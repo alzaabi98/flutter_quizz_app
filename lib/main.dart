5e019bed7401ac1e3d47399c14dd1aad770c8f23
abdulaziz <alzaabi98@gmail.com>
2020-02-13 19:15:48 +0400

    part 2 - building the ui

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,104 @@ class QuestionsPage extends StatefulWidget {
 class _QuestionsPageState extends State<QuestionsPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Quiz App',
-        style: TextStyle(color: Colors.white, fontSize: 32.0),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Text(
+            'Flutter is the best Mobile sdk',
+            style: TextStyle(color: Colors.white, fontSize: 32.0),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: ButtonTheme(
+            minWidth: 200,
+            buttonColor: Colors.white,
+            splashColor: Colors.orange,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text('True', style: TextStyle(fontSize: 32.0)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Expanded(
+          flex: 1,
+          child: ButtonTheme(
+            minWidth: 200,
+            buttonColor: Colors.white,
+            splashColor: Colors.orange,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text('False', style: TextStyle(fontSize: 32.0)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 40.0,
+        ),
+        Expanded(
+          flex: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Correct',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '10',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Wrong',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '2',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
